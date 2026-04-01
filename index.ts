bd759b77c9468c267863c6b87758b3466f2a7e4d
Ayodtrader12 <ibrahimpopoola292@gmail.com>
2026-04-01 12:57:24 +0100

    server

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express, { Application } from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app:Application = express()
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
+})
