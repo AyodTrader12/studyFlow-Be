f042df25a160032d438d5f810d36485c872314d0
Ayodtrader12 <ibrahimpopoola292@gmail.com>
2026-04-29 11:13:58 +0100

    mail service

--- dist/services/EmailService.js
+++ dist/services/EmailService.js
@@ -53,12 +53,13 @@ if (process.env.NODE_ENV === "production") {
     transporter = nodemailer_1.default.createTransport({
         host: process.env.SMTP_HOST, // e.g. "smtp.gmail.com"
         port: parseInt(process.env.SMTP_PORT || "465"),
-        secure: true, // true for port 465 (SSL), false for 587 (TLS)
+        secure: true,
+        service: "gmail", // true for port 465 (SSL), false for 587 (TLS)
         auth: {
             user: process.env.SMTP_USER, // your email address
             pass: process.env.SMTP_PASS, // your app password (NOT your login password)
         },
-        // Force IPv4 (some providers have issues with IPv6)
+        family: 4 // Force IPv4 (some providers have issues with IPv6)
     });
 }
 else {
